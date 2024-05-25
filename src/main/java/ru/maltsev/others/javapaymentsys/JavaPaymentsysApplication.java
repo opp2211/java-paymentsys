4b012ef0b1063097ed4e7c752cfd6ec2755f1272
Kirill Maltsev <opp.2211@gmail.com>
2024-05-25 21:27:13 +0500

    init

---
+++ src/main/java/ru/maltsev/others/javapaymentsys/JavaPaymentsysApplication.java
@@ -0,0 +1,13 @@
+package ru.maltsev.others.javapaymentsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPaymentsysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPaymentsysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/maltsev/others/javapaymentsys/JavaPaymentsysApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.maltsev.others.javapaymentsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPaymentsysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
